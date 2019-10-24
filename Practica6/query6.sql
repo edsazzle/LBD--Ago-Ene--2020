999a37c67f0ce8e27293f866e2385b3e01cd1dcc
Edgar Antonio Sandoval Reza <47533327+edsazzle@users.noreply.github.com>
2019-10-24 15:04:14 -0500

    Add files via upload

---
+++ Practica6/query6.sql
@@ -0,0 +1,31 @@
+use Biblio
+update Usuarios set NombreUsu = 'Gloria'
+where IdUsuario = 1
+
+update Usuarios set ApellidoUsu = 'Sandoval'
+where IdUsuario = 2
+
+update Usuarios set ApellidoUsu2 = 'Rodriguez'
+where IdUsuario = 3
+
+update Usuarios set Direccion = 'Via Lactea'
+where IdUsuario = 4
+
+update Usuarios set Email = 'joncenamacarrones@hotmail.com'
+where IdUsuario = 5
+
+
+delete from Usuarios
+where IdUsuario = 51
+
+delete from Usuarios
+where IdUsuario = 52
+
+delete from Usuarios
+where IdUsuario = 53
+
+delete from Usuarios
+where IdUsuario = 54
+
+delete from Usuarios
+where IdUsuario = 55
